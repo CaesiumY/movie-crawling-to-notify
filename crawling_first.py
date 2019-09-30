00c7323fc7164958aa23b7f19922a08814ef5a7e
caesiumy <mn0658@naver.com>
2019-09-30 23:58:29 +0900

    feat: Add print html text

--- crawling_first.py
+++ crawling_first.py
@@ -1,2 +1,6 @@
 import requests
-from bs4 import Beautifulsoup
+from bs4 import BeautifulSoup
+
+url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0150&date=20191005'
+html = requests.get(url)
+print(html.text)
