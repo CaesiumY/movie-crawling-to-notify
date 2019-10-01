4bb2a12d068eab044d5c1e25be554e38c679844c
caesiumy <mn0658@naver.com>
2019-10-02 01:23:00 +0900

    feat: Add crawling titles

--- crawling_first.py
+++ crawling_first.py
@@ -3,4 +3,9 @@ from bs4 import BeautifulSoup
 
 url = 'http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=0150&date=20191005'
 html = requests.get(url)
-print(html.text)
+
+soup = BeautifulSoup(html.text, 'html.parser')
+list = soup.select('div.info-movie')
+
+for i in list:
+    print(i.select_one('a > strong').text.strip())
